37e1133a31295359ca4134cb67c8b47c6979ac5f
Olivier Broy <olivermasterpower@gmail.com>
2026-06-22 10:14:01 -0400

    parser-fixed

--- src/interpretor/lexer.rs
+++ src/interpretor/lexer.rs
@@ -21,133 +21,70 @@ pub enum Token {
 use std::fs::File;
 use std::io::{Bytes, Read};
 
-pub struct Lexer {
-	bytes: Bytes<File>,
-	stack: Vec<u8>,
-	tokens: Vec<Token>,
-}
-impl Lexer {
-	pub fn tokenize(source: File) -> Result<Vec<Token>, std::io::Error> {
-		let mut lexer = Lexer {
-			bytes: source.bytes(),
-			stack: vec![],
-			tokens: vec![],
-		};
+pub fn tokenize(source: File) -> Result<Vec<Token>, std::io::Error> {
+	let mut bytes = source.bytes();
+	let mut stack = vec![];
+	let mut tokens = vec![];
 
+	while let Some(Ok(bit)) = bytes.next() {
+		if match bit {
+			b'0'..b'9' => true,
+			b'a'..b'z' => true,
+			b'_' => true,
+			_ => false,
+		} == true {
+			stack.push(bit);
+			continue;
+		}
 
-	}
+		if !stack.is_empty() {
+			match &stack[..] {
+				b"let" => tokens.push(Token::Let),
 
-	pub fn token_next(&mut self) -> Token {
-		if let Some(bit) = self.bytes.next() {
-			match bit {
-				// Binary Operator
-				b'+' | b'-' | b'*' | b'/' | b'%' => Token::BinaryOperator(bit),
-				// Eq
-				b'=' => Token::Eq,
-				// Parentheses
-				b'(' => Token::ParentOpen,
-				b')' => Token::ParentClose,
-				// Semicolons
-				b';' => Token::Semi,
-				// White Spaces
-				digit => {
-					self.stack.push(digit);
+				_ => match stack.first().unwrap() {
+					// Numeric
+					b'0'..b'9' => tokens.push(token_num(&mut stack)),
+					// Alphabetic
+					b'a'..b'z' | b'A'..b'Z' | b'_' => tokens.push(token_ident(&mut stack)),
+					x => panic!("Unexpected character: {}", x),
 				},
-				
 			}
-		else { Token::EOF }
+			stack.clear();
+		}
+
+		match bit {
+			// Binary Operator
+			b'+' | b'-' | b'*' | b'/' | b'%' => tokens.push(Token::BinaryOperator(bit)),
+			// Eq
+			b'=' => tokens.push(Token::Eq),
+			// Parentheses
+			b'(' => tokens.push(Token::ParentOpen),
+			b')' => tokens.push(Token::ParentClose),
+			// Semicolons
+			b';' => tokens.push(Token::Semi),
+			// White Spaces
+			b'\r' | b'\t' | b'\n' | b' ' => (),
+			// Digits
+			digit => {
+				stack.push(digit);
+				continue;
+			},
+		};
+		
 	}
+	tokens.push(Token::EOF);
+	Ok(tokens)
 }
 
-pub fn token_num(&mut self) -> Token {
-	let p = self.stack.partition_point(|bit| bit.is_ascii_alphabetic());
-	let digits = String::from_utf8(self.stack[..p]).unwrap();
-	let suffix = String::from_utf8(self.stack[p..]).unwrap();
-	self.stack.clear();
+pub fn token_num(stack: &mut Vec<u8>) -> Token {
+	let p = stack.partition_point(|bit| bit.is_ascii_alphanumeric() || *bit == b'_');
+	let digits = String::from_utf8(stack.drain(..p).collect()).unwrap();
+	let suffix = String::from_utf8(stack.drain(..).collect()).unwrap();
 
 	Token::Numeric { digits, suffix }
 }
+pub fn token_ident(stack: &mut Vec<u8>) -> Token {
+	let ident = String::from_utf8(stack.drain(..).collect()).unwrap();
 
-pub fn token_ident(&mut self)
-
-pub fn tokenize(source: File) -> Result<Vec<Token>, std::io::Error> {
-	let x = source.bytes();
-	let mut tokens = vec![];
-	let mut stack = vec![];
-	let mut stack_pop = || -> Vec<u8> {
-
-	fn process(&mut &mut stack) -> Option<Token> {
-		match tokens {
-			// BinaryExpr
-			b'+' | b'-' | b'*' | b'/' => Some(Token::BinaryOperator(b)),
-
-			// Eq
-			b'=' => Some(Token::Eq),
-
-			// Parentheses
-			b'(' => Some(Token::ParentOpen),
-			b')' => Some(Token::ParentClose),
-
-			// Space
-			b'\n' | b'\r' | b' ' => {
-				if let Some(&b) = stack.first() {
-					match b {
-						b'0'..b'9' => {
-							let mut w: Vec<&[u8]> = stack.split(|b| b.is_ascii_alphabetic()).collect();
-							w.push(&[]);
-
-							let digits = String::from_utf8(w[0].into()).unwrap();
-							let suffix = String::from_utf8(w[1].into()).unwrap();
-
-							stack.clear();
-
-							Some(Token::Numeric { digits, suffix })
-						},
-						_ => match &stack[..] {
-							// Let
-							b"let" => {
-								stack.clear();
-								Some(Token::Let)
-							},
-							// Identifier
-							_ => Some(Token::Ident(String::from_utf8(stack.drain(0..).collect::<Vec<u8>>()).unwrap())),
-						},
-					}
-				} else { None }
-			},
-
-			// Semicolon
-			b';' => Some(Token::Semi),
-
-			// Digit
-			digit => { stack.push(digit); None },
-		}
-	}
-
-	for b in source.bytes() {
-		if let Ok(b) = b {
-			
-			}
-		}
-	}
-	// End
-	if let Some(&b) = stack.first() {
-		match b {
-			b'0'..b'9' => tokens.push({
-				let mut w: Vec<&[u8]> = stack.split(|b| b.is_ascii_alphabetic()).collect();
-				w.push(&[]);
-
-				Token::Numeric {
-					digits: String::from_utf8(w[0].into()).unwrap(),
-					suffix: String::from_utf8(w[1].into()).unwrap(),
-				}
-			}),
-			_ =>  tokens.push(
-				Token::Ident(String::from_utf8(stack.drain(0..).collect::<Vec<u8>>()).unwrap())
-			),
-		};
-	}
-	tokens.push(Token::EOF);
-
-	return Ok(tokens);
+	Token::Ident(ident)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ fn main() -> std::io::Result<()> {
 	let path = env::args().nth(1).expect("Path not provided.");
 	let file = File::open(path).expect("File not accessible.");
 	let tokens = lexer::tokenize(file)?;
+	println!("{:#?}", &tokens);
 
 	let ast = SyntaxTree::new(tokens.into_iter());
-	println!("{:#?}", ast);
+	println!("{:#?}", &ast);
 
 	Ok(())
 }
